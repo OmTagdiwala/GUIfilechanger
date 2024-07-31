5ed8326809b6bb7fbd5631fe6d7aeb70f9dd497e
Om Tagdiwala <om.tagdiwala@gmail.com>
2024-07-31 15:42:34 -0700

    Error message created, file preview code started

--- prim.py
+++ prim.py
@@ -1,21 +1,39 @@
 import tkinter as tk
+from tkinter import messagebox
 kin = tk.Tk()
 print("File Changer")
 filename = ".txt"
 
+
+# # Following code is during file input
+
 def get_file():
     global filename
     filename = filein.get()
+    kin.quit()
 
 kin.title("File Changer :)")
-kin.geometry("900x500")
+kin.geometry("300x50")
 
 tk.Label(kin, text="Enter a Filename: ").grid(row=0)
-filein = tk.Entry(kin)
+# for file input textbox
+filein = tk.Entry(kin, cursor="target")
 filein.grid(row=0, column=1)
 filein.insert(10, ".txt")
-
-tk.Button(kin, text="Search for File", command=get_file).grid(row=2, column=1)
+# for file input confirmation button
+tk.Button(kin, text="Search for File", cursor="dot", command=get_file, width=15, relief="ridge", justify="center").grid(row=2, column=1)
 
 kin.mainloop()
 
+print(filename)
+# # Following code is after file input
+
+if filename == ".txt":
+    print("nooo")
+    messagebox.showerror("Error", "Invalid Filename\nTry Rerunning the program")
+    quit()
+
+file = open(filename, "r")
+# This is the preview of the file
+prepreview = file.readlines()
+preview = f"{prepreview[0]}{prepreview[1]}"
