48d98b7c19298187a4c4755d2b36d40b6e404d03
Om Tagdiwala <om.tagdiwala@gmail.com>
2024-08-04 13:56:55 -0700

    File parse function created, yet to be implemented

--- oswalkmethodlearning.py
+++ oswalkmethodlearning.py
@@ -2,15 +2,35 @@ import os
 
 palist = __file__.split("\\")
 print(palist)
-prim = "\\".join(palist[:2])
+prim = ("\\".join(palist[:2])) + "\\"
 # print(list(os.walk(prim)))
 
-def dirparsing():
+def alldirparsing():
     for root, dirs, files in os.walk(prim):
         for dir in dirs:
             if dir == "":
                 continue
             else:
-                print(dir)
+                print(dir, end="---------")
+            print(root)
 
-dirparsing()
+# print(help(os.walk))
+# alldirparsing()
+
+def scaledirparsing(level=None):
+    global prim
+    if level != None:
+        directoryy = level.split(",")
+        print(directoryy)
+        dirpath = ("\\".join(directoryy)) + "\\"
+        prim = os.path.join(prim, dirpath)
+        print(prim)
+    for i in os.scandir(prim):
+        if not os.path.isdir(i):
+            continue
+        print(i)
+    print(prim)
+
+location = input("Where would you like to search? Seperate directories with commas instead of spaces: ")
+
+scaledirparsing(location)
