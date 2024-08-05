3a49b5c926b6975ed3728f61645e934568a57a00
Om Tagdiwala <om.tagdiwala@gmail.com>
2024-08-04 17:07:32 -0700

    Added tests and adjustments for next phases

--- appliablefilelocator.py
+++ appliablefilelocator.py
@@ -1,27 +1,34 @@
 import os
 import tkinter as tk
 from tkinter import ttk
+selectionlab = "Select File Location"
 kin = tk.Tk()
 palist = __file__.split("\\")
 print(palist)
-machpath = ("\\".join(palist[:2])) + "\\"
+machpath = ("\\".join(palist[:1])) + "\\"
+print(machpath)
 
 q = 5
 
 def addtopath(t):
     global machpath
+    global selectionlab
     machpath = os.path.join(machpath, t)
+    selectionlab = t
+    print(machpath)
+
 
 def file_menus(directories):
     global q
     q+=1
-    mainmenu = ttk.Menubutton(kin, text="Select File Location")
+    selectionlab = "Select File Location"
+    mainmenu = ttk.Menubutton(kin, text=selectionlab)
 
     mainmenu.grid(row= q, column= 1)
 
     firstsubmenu = tk.Menu(mainmenu, tearoff=False)
     for i in directories:    
-        firstsubmenu.add_command(label=i, command= lambda: addtopath(i))
+        firstsubmenu.add_command(label=i, command= lambda i=i: addtopath(i))
 
     mainmenu.configure(menu = firstsubmenu)
     print("cheese", q)
@@ -37,7 +44,12 @@ def dirmenu():
 
 kin.title("File Changer --- File Locator :|")
 kin.geometry("500x150")
+
+
 dirmenu()
 
+print(machpath)
 
 kin.mainloop()
+
+print(machpath)
